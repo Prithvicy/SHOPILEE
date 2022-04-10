7fa6ca453d50d80443dd0395a746d606335b4cac
Prith Kami <prithvi40201@gmail.com>
2022-04-10 09:44:35 +0530

    boiler+firebaseconfig

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDF_jc_UypTdBMC28AwLQjT36LAeF_ZRFA",
+    authDomain: "shopilee.firebaseapp.com",
+    projectId: "shopilee",
+    storageBucket: "shopilee.appspot.com",
+    messagingSenderId: "695931342932",
+    appId: "1:695931342932:web:b6923ef3feb281ac785dc1",
+    measurementId: "G-HQ447TZ8ZG"
+  };
