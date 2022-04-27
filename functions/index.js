07a3c22cddb4e8cb72404ee7c01b84eeaf3d90f3
Prith Kami <prithvi40201@gmail.com>
2022-04-27 05:46:14 +0530

    error debugging

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,38 @@
 const functions = require("firebase-functions");
-const express = require ("express");
+const express = require("express");
 const cors = require("cors");
 const { request, response } = require("express");
 
-const stripe = require ("stripe")('sk_test_51KsSipSCCbalLSeDJlCgpheYCuo4me6hvsOboYc1mqtEEFqm0AFlOBJ8EXFe9okBQoBCmuswRGZLmhdjJuDkiPp600RQrfhLZ6')
-//API
+const stripe = require("stripe")(
+  "sk_test_51KsSipSCCbalLSeDJlCgpheYCuo4me6hvsOboYc1mqtEEFqm0AFlOBJ8EXFe9okBQoBCmuswRGZLmhdjJuDkiPp600RQrfhLZ6"
+);
+//Api...
 
 // App config
-const app= express();
+const app = express();
 
-//Middlewares
-app.use(cors({origin:true}));
+// middlewares
+app.use(cors({ origin: true }));
 app.use(express.json());
 
+// apri routesss
+app.get("/", (request, response) => response.status(200).send("hello world"));
 
-//Apri roots
-app.get('/',(request , response)=> response.status(200).send('hello world'))
-app.post('/payments/create',async (request,response)=>{
-    const total= request.query.total;
-    console.log('Payment request Recived!!for this amount >>>>',total)
-    const paymentIntend = await stripe.paymentIntend.create({
-      amount: total, // subunits of the currency
-      currency: "inr",
-    });
-  
-    // Ok 
-    response.status(201).send({
-      clientSecret: paymentIntend.client_secret,
-    });
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("requested paymetn for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "inr",
   });
 
+  // Ok
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
-
-//listen Commands
-exports.api = functions.https.onRequest(app)
+// this line hear basically listens
+exports.api = functions.https.onRequest(app);
